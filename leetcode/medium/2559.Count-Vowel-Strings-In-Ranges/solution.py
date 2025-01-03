4826db21ce3fdcada1003cc4283f9013e9d92824
Trung PT <thanhtrungit2001@gmail.com>
2025-01-03 15:00:57 +0700

    Update Leetcode problem 2259

--- leetcode/medium/2559.Count-Vowel-Strings-In-Ranges/solution.py
+++ leetcode/medium/2559.Count-Vowel-Strings-In-Ranges/solution.py
@@ -19,6 +19,7 @@ class Solution:
             )
         return ans
 
+
 sol = Solution()
 print(sol.vowelStrings(['aba','bcb','ece','aa','e'], [[0,2],[1,4],[1,1]])) # [2,3,0]
 print(sol.vowelStrings(['a','e','i'], [[0,2],[0,1],[2,2]])) # [3,2,1]
